d908da5ab01b78d0c959d48c906017c4e7f7d4cc
HackerMan <138781082+HackerMan33106@users.noreply.github.com>
2025-02-22 01:58:10 +0300

    Create constants.py

---
+++ v1.1/Structured version/core/constants.py
@@ -0,0 +1,36 @@
+# ANSI escape sequences for colors
+GREEN = "\033[92m"  # Green
+RED = "\033[91m"    # Red
+RESET = "\033[0m"   # Reset color
+PURPLE = "\033[95m" # Purple
+CYAN = "\033[96m"   # Cyan
+
+# Dictionary of interface languages
+LANGUAGES = {
+    "br": "Português (Brasil)",
+    "zh": "中文",
+    "en": "English",
+    "fr": "Français",
+    "de": "Deutsch",
+    "it": "Italiano",
+    "ja": "日本語",
+    "ko": "한국어",
+    "pl": "Polski",
+    "ru": "Русский",
+    "es": "Español"
+}
+
+# Dictionary of localization folder names
+LANGUAGE_FOLDERS = {
+    "br": "Brazilian",
+    "zh": "Chinese",
+    "en": "English",
+    "fr": "French",
+    "de": "German",
+    "it": "Italian",
+    "ja": "Japanese",
+    "ko": "Korean",
+    "pl": "Polish",
+    "ru": "Russian",
+    "es": "Spanish"
+}
